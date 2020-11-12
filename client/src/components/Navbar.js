329eae07b2cf93b0cba6969b406da2fdcb2470e0
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-11-11 22:04:03 -0600

    added homepage layout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
 
   return (
     <header className='navbarContainer'>
-      <NavLink to='/' className='navbarContainer__logo'></NavLink>
+      {/* <NavLink to='/' className='navbarContainer__logo'></NavLink> */}
       <div className='navbarContainer__links'>
         <NavLink className='navbarContainer__links-navlink' to='/login'>Login</NavLink>
         <NavLink className='navbarContainer__links-navlink' to='/signup'>Sign Up</NavLink>
