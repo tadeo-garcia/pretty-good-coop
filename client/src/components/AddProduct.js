c6874f5cf12e993b23b9c64e61f51db570dea13d
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-11-19 19:57:20 -0600

    image preview setup

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 export default function AddProduct(){
+const [title, setTitle] = useState('');
 const [description, setDescription] = useState('');
 const [productImage, setProductImage] = useState(null);
+const [previewImage, setPreviewImage] = useState(null);
 const [price, setPrice] = useState(null);
 const [releaseDate, setReleaseDate] = useState(null);
 // const [sizes, setSizes] = useState(null);
@@ -14,10 +16,24 @@ const handleProfileImage = (e) => {
   });
 };
 
+useEffect(()=>{
+  if(!productImage){
+    setPreviewImage(undefined)
+    return
+  }
+
+  // const imageUrl = URL.createObjectURL(productImage)
+  console.log(productImage)
+  // setPreviewImage(imageUrl)
+
+  // return () => URL.revokeObjectURL(imageUrl)
+},[productImage])
+
   return(
     <div className="addProductWrapper">
       <form className="addProduct__form">
-        <input className="addProduct__form-input" type='text' placeholder='Title'/>
+        <input className="addProduct__form-input" type='text' 
+        onChange={(e)=>setTitle(e.target.value)} placeholder='Title'/>
         <textarea className="addProduct__form-input" placeholder='Description'/>
         <input className="addProduct__form-input" type='number' placeholder='Price'/>
         <div className="addProduct__form-input">
@@ -25,9 +41,17 @@ const handleProfileImage = (e) => {
           <input type='date' placeholder='Release Date'/>
         </div>
         <div className="addProduct__form-input">
-          <input id="addProduct__form-input" type="file" onChange={handleProfileImage}/>
+          <label htmlFor='fileUpload' className='addProduct__form-file'> 
+            Choose File
+            <input id='fileUpload' className="addProduct__form-input" 
+            type="file" title=' ' onChange={handleProfileImage}/>
+          </label>
         </div>
+        <input type="submit" className="addProduct__form-file"/>
       </form>
+      <div className="addProduct__imagepreview">
+        <span>Image Preview</span>
+      </div>
     </div>
   )
 }
