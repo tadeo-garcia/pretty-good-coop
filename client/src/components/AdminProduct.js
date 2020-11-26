f17ddee11892703ebde352de31c5673f6ba95e3d
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-11-26 12:21:04 -0600

    styling for admin products done

--- client/src/components/AdminProduct.js
+++ client/src/components/AdminProduct.js
@@ -8,7 +8,7 @@ export default function AdminProduct(product){
         <img src={product.product.imageUrl} className="adminProductImage"/>
         <h3>{product.product.description}</h3>
         <h3>{product.product.price}</h3>
-        <a onClick={()=>{console.log(product.description, product.price)}} href='#' className="adminProductImage__link adminProductImage__link-animate">Add To Cart</a>
+        <a onClick={()=>{console.log(product.description, product.price)}} href='#' className="adminProductImage__link adminProductImage__link-animate">edit</a>
       </div>
     </>
   )
