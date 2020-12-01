a738d066c4b077721873c9137be5444bc0b93ae0
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-11-30 23:21:25 -0600

    footer desktop/mobile complete

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,14 +13,14 @@ function Footer() {
         </div>
       </div>
       <div id='footerContainer__middle'>
-        {/* Can add something here in the middle */}
+        <a id='footerContainer__link' href='https://github.com/tadeo-garcia/pretty-good-coop' >
+          <i className="fa fa-2x fa-github" aria-hidden="true"></i>
+        </a>
       </div>
       <div id='footerContainer__right'>
-        <div >
-          <a id='footerContainer__link' href='https://instagram.com/prettygoodco_op' >
+        <a id='footerContainer__link' href='https://instagram.com/prettygoodco_op' >
           <i className="fa fa-2x fa-instagram" aria-hidden="true"></i>
-          </a>
-        </div>
+        </a>
       </div>
     </div>
   )
