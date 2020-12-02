c20913cecd2881cac2a4320ae6a1b07b44ca6cdd
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-12-01 23:22:44 -0600

    restructure admin divs

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -14,11 +14,9 @@ export default function Admin(){
     <>
       <div className="adminWrapper">
         <div className="admin__content">
-          <div className="admin__content-right" ref={contentRef}>
-            <div className="displayWrapper">
-              <div className="displayBanner">
-                <div className="admin__content-links">
-                  <Link className="admin__content-link" to='/admin/addproduct'>
+          <div className="displayBanner">
+            <div className="admin__content-links">
+              <Link className="admin__content-link" to='/admin/addproduct'>
                 add product
               </Link>
               <Link className="admin__content-link" to='/admin/viewproducts'>
@@ -27,17 +25,16 @@ export default function Admin(){
               <Link className="admin__content-link" to='/admin/viewarchive'>
                 view archive
               </Link>
-                </div>
-              </div>
-                <Switch>
-                    <Route exact path='/admin/' component={ProductDisplay}/>
-                    <Route exact path='/admin/addproduct' component={AddProduct}/>
-                    <Route exact path='/admin/edit/:id' component={EditProduct}/>
-                    <Route exact path='/admin/viewproducts' component={ProductDisplay}/>
-                </Switch>
-                <div className="displayContent"></div>
-              </div>
             </div>
+          <div className="displayWrapper">
+            <Switch>
+                <Route exact path='/admin/' component={ProductDisplay}/>
+                <Route exact path='/admin/addproduct' component={AddProduct}/>
+                <Route exact path='/admin/edit/:id' component={EditProduct}/>
+                <Route exact path='/admin/viewproducts' component={ProductDisplay}/>
+            </Switch>
+            </div>
+          </div>
         </div>
       </div>
     </>
