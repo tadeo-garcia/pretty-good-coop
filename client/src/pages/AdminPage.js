fa5fb2cf2968f1fc16433d190d6161f657fc5936
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-12-01 23:38:09 -0600

    added admin links to admin navbar

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,14 +1,20 @@
 import React, {useRef} from "react";
-// import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Link, Switch, Route } from "react-router-dom";
 import AddProduct from '../components/AddProduct';
 import EditProduct from '../components/EditProduct';
 import ProductDisplay from '../components/ProductDisplay';
+import {logout} from '../store/actions/authActions'
 
 
 export default function Admin(){
   const contentRef = useRef();
+  const dispatch = useDispatch();
   
+   const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
 
   return(
     <>
@@ -16,14 +22,33 @@ export default function Admin(){
         <div className="admin__content">
           <div className="displayBanner">
             <div className="admin__content-links">
-              <Link className="admin__content-link" to='/admin/addproduct'>
+              <Link className="admin__content-link" 
+                to='/admin/addproduct'>
                 add product
               </Link>
-              <Link className="admin__content-link" to='/admin/viewproducts'>
+              <Link className="admin__content-link" 
+                to='/admin/viewproducts'>
                 view products
               </Link>
-              <Link className="admin__content-link" to='/admin/viewarchive'>
+              <Link className="admin__content-link" 
+                to='/admin/orders'>
+                view orders
+              </Link>
+              <Link className="admin__content-link" 
+                to='/admin/viewarchive'>
                 view archive
+              </Link>
+              <Link className="admin__content-link" 
+                to='/home'>
+                home
+              </Link>
+              <Link className="admin__content-link" 
+                to='/shop'>
+                shop
+              </Link>
+              <Link className="admin__content-link" 
+                to='/' onClick={handleLogout} >
+                logout
               </Link>
             </div>
           <div className="displayWrapper">
