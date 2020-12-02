b77a68b55ce61dac1229e41bec8769cfcbb81b79
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-12-02 01:19:59 -0600

    restructured homepage to userpage, rendering content through dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,9 @@
 import React from "react";
 // import { useSelector } from "react-redux";
 import {  Switch, Route } from "react-router-dom";
-import ProductDisplay from "../components/ProductDisplay"
+import ProductDisplay from "./ProductDisplay";
+import About from "./About";
+import Splash from "./Splash";
 
 export default function Dashboard(){
   
@@ -10,7 +12,9 @@ export default function Dashboard(){
       <div className="dashboardWrapper">
         <div id="dashboard__content">
             <Switch>
+              <Route to='/' component={Splash}/>
               <Route exact path="/dashboard" component={ProductDisplay} />
+              <Route exact path="/about" component={About} />
             </Switch>
           </div>
       </div>
