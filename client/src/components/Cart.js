e031df694a8a56e8cdc718c2a821cccf16a5bf68
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-12-07 22:53:22 -0600

    cart layout modified

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import CartItem from './CartItem'
 
 export default function Cart(){
   
   return(
     <div className="cartWrapper">
-      <div className="cartItems"></div>
+      <div className="cartItems">
+      <CartItem/>
+      <CartItem/>
+      <CartItem/>
+
+      </div>
       <div className="cartTotals"></div>
     </div>
   )
--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Product(product){
-  let singleProduct = product.product;
+export default function CartItem(product){
+  let singlecartItem = product.product
  
   return(
     <>
       <div className="cartItemWrapper">
-        <img src={singlecartItem.imageUrl} 
+        {/* <img src={singlecartItem.imageUrl} 
         className="cartItemImage" alt='cartItem'/>
         <Link to='/cart'  
-        onClick={()=>{console.log(singleProduct.description, singleProduct.price)}}  
+        onClick={()=>{console.log(singlecartItem.description, singlecartItem.price)}}  
         className="cartItemImage__link cartItemImage__link-animate">
           add to cart
         </Link>
         <div className="cartItemDescription">
-          {singleProduct.description}
+          {singlecartItem.description}
         </div >
         <div className="cartItemPrice">
-          ${Math.round((singleProduct.price * 100)/100).toFixed(2)}
-        </div >
+          ${Math.round((singlecartItem.price * 100)/100).toFixed(2)}
+        </div > */}
       </div>
     </>
   )
