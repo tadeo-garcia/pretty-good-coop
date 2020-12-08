783f10c266bab26ae0fa25d05cddb241bd45c05d
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-12-07 22:06:56 -0600

    added floating cart component and css file

---
+++ client/src/components/Cart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CartTotal(){
+  return (
+    <div className='cartTotal'>
+      <i class="fa fa-shopping-cart" aria-hidden="true"></i> The Total Goes here
+    </div>
+  )
+}
