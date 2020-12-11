9b7dc7288085b7cdfc2a5a7c53fa048f862452fb
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-12-10 22:19:56 -0600

    restyled cart bugs for mobile/desktop views

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -4,35 +4,17 @@ import { Link } from 'react-router-dom';
 export default function CartItem(props){
   let singlecartItem = props.product;
   let desktop = props.desktop;
-  return(
-    <>
-      {desktop?(
-        <div className="cartItemWrapper">
-          <img src={singlecartItem.imageUrl} 
-          className="cartItemImage" alt='cartItem'/>
-          <div className="cartItemDescription">
-            {singlecartItem.description}
-            <button className="cartItemRemove">remove item</button>
-          </div>
-          <div className="cartItemPrice">
-            ${Math.round((singlecartItem.price * 100)/100).toFixed(2)}
-          </div>
-        </div>
-     ):(
-        <div className="cartItemWrapper">
-          <img src={singlecartItem.imageUrl} 
-          className="cartItemImage" alt='cartItem'/>
-          <div className="cartItemDescription-mobile">
-            <div className="cartItemDescription">
-              {singlecartItem.description}
-            <button className="cartItemRemove">remove item</button>
-            </div>
-            <div className="cartItemPrice">
-              ${Math.round((singlecartItem.price * 100)/100).toFixed(2)}
-            </div>
-          </div>
-        </div>
-      )}
-    </>
+  return (
+    <div className="cartItemWrapper">
+      <img src={singlecartItem.imageUrl} 
+      className="cartItemImage" alt='cartItem'/>
+      <div className="cartItemDescription">
+        {singlecartItem.description}
+      <div className="cartItemPrice">
+        ${Math.round((singlecartItem.price * 100)/100).toFixed(2)}
+      </div>
+        <button className="cartItemRemove">remove item</button>
+      </div>
+    </div>
   )
 }
