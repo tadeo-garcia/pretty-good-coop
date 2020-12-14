b9bb46f0b960d666ae158a87432f5c9344ead62f
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-12-13 22:19:19 -0600

    added two more actions and thunks for load orders by user/admin

--- client/src/store/actions/orderActions.js
+++ client/src/store/actions/orderActions.js
@@ -1,16 +1,33 @@
 import {
   export const LOAD_ORDER = 'LOAD_ORDER';
+  export const LOAD_ORDERS_BY_USER = 'LOAD_ORDERS_BY_USER';
+  export cosnt LOAD_ORDERS_ALL = 'LOAD_ORDER_ALL'
   export const CREATE_ORDER = 'CREATE_ORDER';
   export const DELETE_ORDER = 'DELETE_ORDER';
 } from '../constants/orderConstants';
 
 //////////////ACTIONS/////////////////////
-const loadOrderData = (orderId) => {
+const loadOrderById = (orderId) => {
   return{
     type: LOAD_ORDER,
     orderId
   }
 }
+
+const loadOrdersByUser = (userOrders) => {
+  return{
+    type: LOAD_ORDERS_BY_USER,
+    userOrders
+  }
+}
+
+const loadOrdersAdmin = (orders) => {
+  return{
+    type: LOAD_ORDERS_ALL,
+    orders
+  }
+}
+
 
 const addOrder = (order) => {
   return{
@@ -27,11 +44,25 @@ const deleteOrderByIdx = (orderId) => {
 }
 
 //////////////THUNKS/////////////////////
+
 export const loadOrder = (orderId) => {
   return async (dispatch) => {
-    dispatch(loadOrderData(orderId))
+    dispatch(loadOrderById(orderId))
   }
 }
+
+export const loadOrders = (userId) => {
+  return async (dispatch) => {
+    dispatch(loadOrdersByUser(userId))
+  }
+}
+
+export const loadOrdersAdmin = (orders) => {
+  return async (dispatch) => {
+    dispatch(loadOrdersAdmin(orders))
+  }
+}
+
 
 export const createOrder = (order) => {
   return async (dispatch) => {
